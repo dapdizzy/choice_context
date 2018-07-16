c5700253d9e7ffb7757392d96f370275113006c9
pyatkov <pyatkov@media-saturn.com>
2018-07-16 13:26:36 +0300

    Implemented a neat version.

--- lib/choice_context.ex
+++ lib/choice_context.ex
@@ -1,11 +1,17 @@
 defmodule ChoiceContext do
-  defstruct [:mode]
+  import Kernel, except: [&&: 2]
+
+  defstruct [:mode, :choices, :function]
 
   use GenServer
 
   # API
-  def start_link(timeout) do
-    __MODULE__ |> GenServer.start_link(timeout, name: __MODULE__)
+  def start_link(timeout, %{} = choices, function) when function |> is_function(1) do
+    __MODULE__ |> GenServer.start_link({timeout, choices, function}, name: __MODULE__)
+  end
+
+  def start(timeout, %{} = choices, function) when function |> is_function(1) do
+    __MODULE__ |> GenServer.start({timeout, choices, function}, name: __MODULE__)
   end
 
   def accept(server \\ __MODULE__, choice) do
@@ -13,20 +19,87 @@ defmodule ChoiceContext do
   end
 
   # Callbacks
-  def init(timeout) do
+  def init({timeout, choices, function}) do
     self() |> Process.send_after(:timeout, timeout)
-    {:ok, %__MODULE__{mode: :active}}
+    {:ok, %__MODULE__{mode: :active, choices: choices, function: function}}
   end
 
-  def handle_call({:accept, choice}, _from, %__MODULE__{mode: :active} = state) do
-    {:stop, :normal, {:choice, choice}, state}
+  def handle_call({:accept, option}, _from, %__MODULE__{mode: :active, choices: choices, function: function} = state) do
+    {options, counter} = parse_option(option)
+    result =
+      case counter do
+        1 ->
+          [choice] = options
+          choice |> process_choice(function, choices) |> unwrap()
+        n when n |> is_integer() and n > 1->
+          {processed, not_processed} =
+            options |> Stream.map(fn option ->
+              option |> process_choice(function, choices)
+            end) |> Enum.split_with(fn processing_result ->
+              case processing_result do
+                {:ok, _result} -> true
+                {:invalid_option, _description} -> false
+              end
+            end)
+          processed_result = processed |> Stream.map(& unwrap(&1)) |> Enum.join("\n")
+          not_processed_result = with x <- not_processed |> Stream.map(& unwrap(&1)) |> Enum.join("\n"), do: x && (x <> "\n" <> valid_options_string(choices))
+          (processed_result && (processed_result <> (not_processed_result && "\n"))) <> not_processed_result
+      end
+    {:stop, :normal, {:result, result}, state}
   end
+
+  # def handle_call({:accept, _option}, _from, %__MODULE__{mode: :active, choices: choices} = state) do
+  #   {:reply, {:reply, "Valid options are: *#{choices |> Map.keys() |> Enum.join(~S|, |)}*"}, state}
+  # end
 
   def handle_call(_, _from, %__MODULE__{mode: :inactive} = state) do
-    {:stop, :normal, {:reply, "Too late, the choise context has ended."}, state}
+    {:stop, :normal, {:reply, "Too late, the choice context has ended."}, state}
   end
 
   def handle_info(:timeout, state) do
     {:noreply, %{state|mode: :inactive}}
   end
+
+  def parse_option(option) do
+    option |> String.split(",", trim: true) |> Stream.map(& String.trim(&1))
+      |> Enum.map_reduce(0, fn x, counter -> {x, counter + 1} end)
+  end
+
+  def choose_option(option, map) do
+    case map do
+      %{^option => choice} ->
+        {:choice, choice}
+      _ ->
+        :invalid_option
+    end
+  end
+
+  def process_choice(choice, function, choices) do
+    case choice |> choose_option(choices) do
+      {:choice, choice} ->
+        {:ok, function.(choice)}
+      :invalid_option ->
+        {:invalid_option, "Option *#{choice}* is invalid."}
+    end
+  end
+
+  def valid_options_string(choices) do
+    "Valid options are: *#{choices |> Map.keys() |> Enum.join(~S|, |)}*."
+  end
+
+  def unwrap(processing_result) do
+    case processing_result do
+      {:ok, result} -> result
+      {:invalid_option, description} -> description
+    end
+  end
+
+  defp swallow(a, b) do
+    case a do
+      "" -> b
+      _ -> Kernel.&&(a, b)
+    end
+  end
+
+  defp a && b, do: swallow(a, b)
 end
--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
-defmodule ChaoiseContext.MixProject do
+defmodule ChoiseContext.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :choise_context,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "ChoiceContext"
     ]
   end
 
@@ -21,8 +24,26 @@ defmodule ChaoiseContext.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    """
+    This is a GenServer-ish implementation of a Choise Context.
+    That is a process that accepts a value during the specified period of time and refuses to do so after the timeout has elapsed.
+    """
+  end
+
+  defp package do
+    [
+      name: "choice_context",
+      maintainers: ["Dmitry A. Pyatkov"],
+      licenses: ["Apache 2.0"],
+      files: ["lib", "mix.exs"],
+      links: %{"HexDocs.pm" => "https://hexdocs.pm"}
     ]
   end
 end
